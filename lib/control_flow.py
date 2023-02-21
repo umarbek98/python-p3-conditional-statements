7e0bd5e86798956260458d5cb9f062748413546d
Umarbek Rakhmatov <umarbek1306@gmail.com>
2023-02-21 18:29:23 -0500

    completed lab

--- lib/control_flow.py
+++ lib/control_flow.py
@@ -1,17 +1,51 @@
 #!/usr/bin/env python3
 
 def admin_login(username, password):
-    # your code here
-    pass
+    if username == 'sudo' and password == '12345':
+        print('Access denied')
+        return 'Access denied'
+    elif username == 'admin' and password == '12345':
+        return 'Access granted'
+    elif username == 'ADMIN' and password == '12345':
+        return 'Access granted'
+    else : 
+        return 'Access denied'
+
 
 def hows_the_weather(temperature):
-    # your code here
-    pass
+    if temperature < 40:
+        return "It's brisk out there!"
+    elif temperature >= 40 and temperature <= 65:
+        return "It's a little chilly out there!"
+    elif temperature > 86:
+        return "It's too dang hot out there!"
+    else :
+        return "It's perfect out there!"
 
 def fizzbuzz(num):
-    # your code here
-    pass
+    if not num % 3 and not num % 5:
+        print('FizzBuzz')
+        return "FizzBuzz"
+    elif not num % 5:
+        print('Buzz')
+        return 'Buzz'
+    elif not num % 3:
+        print('Fizz')
+        return "Fizz"
+    else : 
+        print(num)
+        return num
+fizzbuzz(15)
 
 def calculator(operation, num1, num2):
-    # your code here
-    pass
+    if operation == '+':
+        return num1 + num2
+    elif operation == '-':
+        return num1 - num2
+    elif operation == '*':
+        return num1 * num2
+    elif operation == "/":
+        return num1 / num2
+    else :
+        print("Invalid operation!")
+        return None
